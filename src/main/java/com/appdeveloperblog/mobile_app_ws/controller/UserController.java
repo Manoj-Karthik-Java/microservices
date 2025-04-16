c364895aecf5e8a2b36e28351186127ad60e15ef
manoj-karthik <vemuri.manojkarthik178@gmail.com>
2025-04-16 22:45:38 +0530

    add controller class for user with @GetMapping annotation and query params

--- src/main/java/com/appdeveloperblog/mobile_app_ws/controller/UserController.java
+++ src/main/java/com/appdeveloperblog/mobile_app_ws/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.appdeveloperblog.mobile_app_ws.controller;
 
+import com.appdeveloperblog.mobile_app_ws.model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,9 +9,29 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
 //    Below annotation can also be written as @GetMapping("/{userId}")
-    @GetMapping(params = "/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "get mapping";
+//    produces property is used to produce return data in xml format
+    @GetMapping(path = "/{userId}",produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUser(@PathVariable String userId){
+        UserRest userRest = new UserRest();
+        userRest.setEmail("test@test.com");
+        userRest.setFirstName("manoj");
+        userRest.setLastName("karthik");
+        return userRest;
+    }
+
+    /*
+        If we want optional value for query parameters then we need to use 'required = false' property
+        we need to assign default value for query params because in the below example page and limit are primitives,
+        primitives cannot be assigned to null.
+        If we do not want to assign default value it is safe to not use primitives while defining query params
+     */
+    @GetMapping
+    public String getUsers(
+            @RequestParam(value = "page",defaultValue = "1") int page,
+            @RequestParam(value = "limit",defaultValue = "50") int limit,
+            @RequestParam(value = "sort",defaultValue = "desc",required = false) String sort
+    ){
+        return page + " " + limit;
     }
 
 }
